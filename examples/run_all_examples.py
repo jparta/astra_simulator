f26dd8a2ea58df5facb151c6e5de008361f5ec5d
Joonatan Partanen <joonatan.partanen@iceye.fi>
2023-04-21 17:51:52 +0300

    Add script to run all examples

---
+++ examples/run_all_examples.py
@@ -0,0 +1,16 @@
+# Run all python files in examples/
+import os
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    containing_dir = Path(__file__).parent
+    containing_dir = containing_dir.relative_to(containing_dir.parent)
+    script_logs = containing_dir / "logs"
+    script_logs.mkdir(exist_ok=True)
+
+    for file in containing_dir.glob("example_*.py"):
+        logs_file = script_logs / f"{file.stem}.log"
+        print(f"Running {file} with output to {logs_file}")
+        exit_status = os.system(f"python {file} > {logs_file} 2>&1")
+        print(f"Exit status: {exit_status}")
